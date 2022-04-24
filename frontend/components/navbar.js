825dd6fd2b9984a5482068d22d58975dbe8590b5
Your Namepoomin12082542 <poomin12082542@hotmail.com>
2022-04-24 17:29:06 +0700

    13th commit change logout botton

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => (
         <Link href="/register"><li className ="text-xl font-sans p-2 hover:text-gray-500 mx-6 mb-2   uppercase tracking-widest"> Register </li></Link>  
         <Link href="/gallery"><li className ="text-xl font-sans p-2 hover:text-gray-500 mx-6 mb-2   uppercase tracking-widest"> Your Gallery </li></Link>  
         <Link href="/aboutus"><li className ="text-xl font-sans p-2 hover:text-gray-500 mx-6 mb-2text-xs uppercase tracking-widest"> About Us</li></Link> 
-        <Link href="/logout"><li className ="text-xl font-sans p-2 hover:text-gray-500 mx-6 mb-2  uppercase tracking-widest"> Logout </li></Link> 
+        <Link href="/logout"><li className ="text-xl font-sans p-2 text-red-700 hover:text-red-500 mx-6 mb-2 mt-28  uppercase tracking-widest"> Logout </li></Link> 
         </ul>    
     </div>
 )
